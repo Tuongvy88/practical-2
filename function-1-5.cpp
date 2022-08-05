b7a0303b5006de5cf61c4eabef162dd4a5feb653
Tuongvy88 <109933390+Tuongvy88@users.noreply.github.com>
2022-08-05 22:55:48 +1000

    Add files via upload

--- function-1-5.cpp
+++ function-1-5.cpp
@@ -1,13 +1,16 @@
-#include <iostream>
-using namespace std;
-
-void print_summed(int array1[3][3],int array2[3][3]) {
-    for (int i = 0; i<3; i++) {
-        int sum = 0;
-        for (int j = 0; j<3; j++) {
-            sum = array1[i][j] + array2[i][j];
-            std::cout << sum << " ";
+#include<iostream>
+void print_summed(int array1[3][3],int array2[3][3]){
+    for(int i=0; i<3;i++){
+        for(int j=0; j<3; j++){
+            array1[i][j] += array2[i][j];
+            if(j == 2){
+                 std::cout<<array1[i][j];
+            }
+            else{
+                    std::cout<<array1[i][j]<<" ";       
+            }
         }
-        std::cout<<std::endl;
+        std::cout<<"\n"; 
+
+        }
     }
-}
--- main-1.5.cpp
+++ main-1.5.cpp
@@ -1,10 +1,13 @@
-#include <iostream>
-#include <stdlib.h>
-extern void print_summed(int array1[3][3],int array2[3][3]);
 
-int main (int argc, char** argv) {
+#include<iostream>
+
+using namespace std;
+void print_summed(int array1[3][3],int array2[3][3]);
+
+int main(){
     int matrix1[3][3] = {{0,1,2},{3,4,5},{6,7,8}} ;
     int matrix2[3][3] = {{0,0,0},{2,2,2},{-5,-4,8}} ;
     print_summed(matrix1,matrix2) ;
+
     return 0;
 }
